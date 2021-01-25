8e007693191da607f7f5b13810cb358f4dd069a1
Bhavesh <bhaveshbhai_vaviya@persistent.com>
2021-01-25 14:58:57 +0530

    Added math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+# Perform Additon !
+def add(x,y):
+    return x+y
+
+# Perform Substraction
+def sub(x,y):
+    if x<y:
+        return "ERROR"
+    return x-y
+
+# Perform Multiplication
+def mul(x,y):
+    return x*y
+
+# Perform Division
+def div(x,y):
+    if y==0:
+        return ERROR
+    return x/y
+
+def square(x):
+    return x*x
